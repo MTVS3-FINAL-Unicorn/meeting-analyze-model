bc1733a3c30a7574b4a8892933713f7cec7fafaa
2realzoo <realzoojin@gmail.com>
2024-11-09 23:38:52 +0900

    chore: update docstring, change tokenize_text func's return type and remove_stopwords func's input type

--- AnalyzeMeeting/text_organize.py
+++ AnalyzeMeeting/text_organize.py
@@ -1,18 +1,32 @@
 import re
-
+from typing import Union, List
 from konlpy.tag import Okt
 
 
-def tokenize_text(text, token_len=2):
-    text = re.sub(r'[^ㄱ-ㅣ가-힣\s]', '', text)
-    okt = Okt()
-    okt_data = okt.pos(text)
-    word_list = [word for word, pos in okt_data if (pos in ['Noun', 'Adjective']) and (len(word) >= token_len) ]
-    sentence = ' '.join(word_list)
-    return sentence
+def tokenize_text(text: Union[str, List[str]], token_len: int = 2) -> list:
+    """ 한국어 문장을 konlpy okt로 토큰화하여 공백으로 합쳐진 문자열을 반환합니다.
 
-def remove_stopwords(text):
-    tokens = text.split(' ')
+    Args:
+        text (_type_):  str | list
+        token_len (int, optional):  int, Defaults to 2.
+
+    Returns:
+        _type_: str
+    """
+    okt = Okt()
+
+    # 입력이 리스트일 경우
+    if isinstance(text, list):
+        tokens = [token for sentence in text for token in tokenize_text(sentence, token_len=token_len)]
+        return tokens
+
+    # 입력이 문자열일 경우
+    text = re.sub(r'[^ㄱ-ㅣ가-힣\s]', '', text)
+    okt_data = okt.pos(text)
+    word_list = [word for word, pos in okt_data if (pos in ['Noun', 'Adjective']) and (len(word) >= token_len)]
+    return word_list
+
+def remove_stopwords(tokens:List[str]) -> list:
     stopwords = [
     "이", "그", "저", "을", "를", "은", "는", "이다", "있다", "없다", "에", "에서", 
     "와", "과", "로", "으로", "의", "도", "에게", "한테", "그리고", "그러나", 
